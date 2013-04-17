b759f2deb97480d6bbef712c2d83892ba63a3e8f
Victor Ng <victor@crankycoder.com>
2013-04-17 14:36:47 -0400

    CEF and sentry working end to end now

--- demo.js
+++ demo.js
@@ -5,60 +5,28 @@ var heka = require('heka');
 var os = require('os');
 var sys = require('util');
 var strftime = require('strftime');
+
 var cef = require('cef');
+var raven = require('raven');
+
+var get_heka_client = function (sender) {
+    var config = {
+        'sender': sender,
+        'logger': 'test',
+        'severity': 5
+    };
+    var jsonConfig = JSON.stringify(config);
+    var heka_client = heka.clientFromJsonConfig(jsonConfig);
+    return heka_client;
+}
+
 
 var get_cef = function(heka_client) {
     var some_func = function(sample) {
         this._capture = sample;
     };
 
-    var custom_factory = function(syslog_config) {
-        var requiredHekaSyslogOptions = ['syslog_options',
-                                         'syslog_facility',
-                                         'syslog_ident',
-                                         'syslog_priority'];
-        var self = this;
-
-        // Bind in the syslog configuration
-        self._syslog_config = {};
-        // The only syslog options we really care about are the
-        // ident and facility fields
-        self._syslog_config['syslog_ident'] = syslog_config.tag || "node_cef";
-        self._syslog_config['syslog_facility'] = syslog_config.facility|| "local4";
-        self._syslog_config['syslog_priority'] = "";
-        self._syslog_config['syslog_options'] = "";
-
-
-        // A replacement object for syslog which transports over
-        // heka instead
-        var heka_not_syslog = {
-            heka_transport: function(){
-                return heka_client;
-            },
-            log: function(message, severity) {
-                this._transport_capture_msg = message;
-                this._transport_capture_severity = severity;
-
-                var timestamp = new Date();
-                var hostname = os.hostname();
-
-                message = strftime.strftimeUTC("%b %d %H:%M:%S", timestamp) + " " + hostname + " " + message;
-
-                var heka_cef_opts = {fields: {cef_meta: self._syslog_config},
-                                     payload: message,
-                                     timestamp: timestamp,
-                                     severity: severity};
-                this.heka_transport().heka('cef', heka_cef_opts);
-            },
-            syslog_config: function() {
-                /*
-                 * This provides access to the identity tag and the facility level
-                 */
-                return self._syslog_config;
-            }
-        }
-        return heka_not_syslog;
-    };
+    var cef_log_factory = heka.Adapters.cef_log_factory(heka_client);
 
     var config = {
         vendor: 'Mozilla',
@@ -66,27 +34,56 @@ var get_cef = function(heka_client) {
         version: '0.1-baz',
         syslog_tag: 'addons-stage',
         syslog_facility: 'local4',
-        log_factory: custom_factory,
+        log_factory: cef_log_factory,
         err_back: some_func
     };
     return cef.getInstance(config);
 }
 
-function main() {
-    var config = {
-        'sender': {'factory': './senders:stdoutSenderFactory'},
-        'logger': 'test',
-        'severity': 5
-    };
-    var jsonConfig = JSON.stringify(config);
-    var heka_client = heka.clientFromJsonConfig(jsonConfig);
 
+function test_raven(heka_client) {
+    var get_raven = function(dsn, heka_client) {
+        var HekaRavenTransport = heka.Adapters.HekaRavenTransport;
+        var opt = {transport: new HekaRavenTransport(heka_client)};
+        var client = new raven.Client(dsn, opt)
+        return client;
+    }
+
+    var Foo = function() {
+    }
+
+    Foo.prototype.recursive_error = function(counter) {
+        var some_number = Math.random();
+        if (counter === 0) {
+            return new Error("Something bad happened!");
+        }
+        var result = this.recursive_error(counter-1);
+        return result;
+    }
+
+    var dsn = "udp://974ae34313ac4560a6eabb2b26f0d567:ffe16afe86ea4245bda99e02d4762e98@localhost:9001/2";
+    var client = get_raven(dsn, heka_client);
+    var f = new Foo();
+    var err = f.recursive_error(50);
+    client.captureError(err);
+}
+
+var test_cef = function(heka_client) {
     var cef_client = get_cef(heka_client);
-    cef_client.info({name: "something wacky",
-                     signature: 1234});
+    cef_client.info({name: "something wacky", signature: 1234});
+}
+
+function main() {
+
+    var stdoutSender = {'factory': './senders:stdoutSenderFactory'};
+    var udpSender = {'factory': './senders:udpSenderFactory',
+               'hosts': '0.0.0.0',
+               'ports': 5565};
+
+    var heka_client = get_heka_client(udpSender);
+    //test_cef(heka_client);
+    test_raven(heka_client);
 };
-
-
 
 if (process.argv[1] === __filename) {
   main();
